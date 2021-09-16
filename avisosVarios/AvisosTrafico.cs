c156f59e6d6f725555289a450e7cf8fd197d46af
Fede Antonelli <antonelli.fd@gmail.com>
2021-09-16 14:21:21 -0300

    Ejercicio avisos

---
+++ avisosVarios/AvisosTrafico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace avisosVarios
+{
+	class AvisosTrafico : IAvisos
+	{
+		private string remitente;
+		private string mensaje;
+		private string fecha;
+
+		public AvisosTrafico()
+		{
+			remitente = "La Policia";
+			mensaje = "Sancion cometida.. si abona antes recibira un descuento..";
+			fecha = "de HOY";
+		}
+		public AvisosTrafico(string remitente, string mensaje, string fecha) 
+		{
+			this.remitente = remitente;
+			this.mensaje = mensaje;
+			this.fecha = fecha;
+		}
+
+		public string GetFecha() => fecha;
+		public void MostrarAviso() => Console.WriteLine("Mensaje: {0} enviado por: {1} el dia {2}", mensaje, remitente, fecha);
+	}
+}
---
+++ avisosVarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//Interfaces - objetos con parametros y sin parametros
+namespace avisosVarios
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Aviso sin parametros
+			Console.WriteLine("***PRIMER AVISO***");
+			AvisosTrafico aviso1 = new AvisosTrafico();
+
+			aviso1.MostrarAviso();
+
+			//Aviso con parametros
+			Console.WriteLine("\n***SEGUNDO AVISO***");
+			AvisosTrafico aviso2 = new AvisosTrafico("Prefectura", "Vas preso", "31/03/1999");
+ 
+			aviso2.MostrarAviso();
+		}
+	}
+}
